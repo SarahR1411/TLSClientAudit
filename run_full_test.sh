2b431cba26313b314ed64e5d44a005471c067f6c
SarahR1411 <sarahrmili1411@gmail.com>
2026-01-19 09:20:30 +0100

    Remove insecure mode use in local script to use mitmproxy's CA

--- run_full_test.sh
+++ run_full_test.sh
@@ -15,19 +15,19 @@ echo "*** Starting script ***"
 
 echo ""
 echo ">>> Triggering connection 1 (Baseline Analysis)..."
-curl --proxy http://127.0.0.1:8080 https://www.google.com -k -s -o /dev/null
+curl --proxy http://127.0.0.1:8080 --cacert ~/.mitmproxy/mitmproxy-ca-cert.pem https://www.google.com -s -o /dev/null
 
 echo ""
 echo ">>> Triggering connection 2 (TLS 1.0 Attack)..."
-curl --proxy http://127.0.0.1:8080 https://www.google.com -k -s -o /dev/null
+curl --proxy http://127.0.0.1:8080 --cacert ~/.mitmproxy/mitmproxy-ca-cert.pem https://www.google.com -s -o /dev/null
 
 echo ""
 echo ">>> Triggering connection 3 (Weak Cipher Attack)..."
-curl --proxy http://127.0.0.1:8080 https://www.google.com -k -s -o /dev/null
+curl --proxy http://127.0.0.1:8080 --cacert ~/.mitmproxy/mitmproxy-ca-cert.pem https://www.google.com -s -o /dev/null
 
 echo ""
 echo ">>> Triggering connection 4 (Bad Certificate Test)..."
-curl --proxy http://127.0.0.1:8080 https://www.google.com -k -s -o /dev/null
+curl --proxy http://127.0.0.1:8080 --cacert ~/.mitmproxy/mitmproxy-ca-cert.pem https://www.google.com -s -o /dev/null
 
 # cleanup (stops the tool)
 kill $MITM_PID
